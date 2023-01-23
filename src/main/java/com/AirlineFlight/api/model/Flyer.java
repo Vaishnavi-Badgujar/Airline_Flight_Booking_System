e52d03316a44a7392bf952d9c8238ad2c8ec15a5
Chandrakant Badgujar <vaishnavi.chandrakant-badgujar@capgemini.com>
2023-01-23 14:36:19 +0530

    first commit

---
+++ src/main/java/com/AirlineFlight/api/model/Flyer.java
@@ -0,0 +1,8 @@
+package com.AirlineFlight.api.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Flyer {
+
+}
---
+++ src/test/java/com/AirlineFlight/api/AirlineFlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AirlineFlight.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineFlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
