61df4708a67af31b985062c2af8a93dca74b49d3
Chandrakant Badgujar <vaishnavi.chandrakant-badgujar@capgemini.com>
2023-01-23 22:25:21 +0530

    Added

--- src/main/java/com/AirlineFlight/api/Service/FlightService.java
+++ src/main/java/com/AirlineFlight/api/Service/FlightService.java
@@ -1,5 +1,38 @@
 package com.AirlineFlight.api.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.AirlineFlight.api.model.Flight;
+
+@Service
 public class FlightService {
+
+	public void insertFlight(Flight flight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Flight> getAllFlight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<Flight> getFlightById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateFlightById(Flight flight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteFlightById(Flight flight) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/com/AirlineFlight/api/controller/FlightController.java
+++ src/main/java/com/AirlineFlight/api/controller/FlightController.java
@@ -1,5 +1,69 @@
 package com.AirlineFlight.api.controller;
 
-public class FlightController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.AirlineFlight.api.Service.FlightService;
+import com.AirlineFlight.api.model.Flight;
+
+@RestController
+@RequestMapping("/api/flight")
+public class FlightController {
+	
+	@Autowired
+	private FlightService flightService;
+	
+	// Flight POST API
+	
+	@PostMapping("/add")
+	public ResponseEntity<String> postFlight(@RequestBody Flight flight) {
+		flightService.insertFlight(flight);
+		return ResponseEntity.status(HttpStatus.OK).body("Flight Posted in DB");
+		
+	}
+	
+	// Flight ADD API
+	
+	@GetMapping("/getall")
+	public List<Flight> getAllFlight(){
+		List<Flight> list = flightService.getAllFlight();
+		return list;
+	}
+	
+	// Get Flight by ID
+	
+	@GetMapping("/one/{id}")
+	public ResponseEntity<Object> getFlightById(@PathVariable("id") int id) {
+		Optional<Flight> optional = flightService.getFlightById(id);
+		if (!optional.isPresent())
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid ID given");
+		
+		Flight flight = optional.get();
+		return ResponseEntity.status(HttpStatus.OK).body(flight);
+	}
+
+	
+	//put Flight API
+	
+	@PutMapping("/one/{id}")
+	public ResponseEntity<String> updateFlightById (@PathVariable("id") int id,@RequestBody Flight flight) {
+		flightService.updateFlightById(flight);
+		return ResponseEntity.status(HttpStatus.OK).body("Flight is updated");
+		}
+	
+	public ResponseEntity<String> deleteFlightById(@PathVariable("id") int id,@RequestBody Flight flight) {
+		flightService.deleteFlightById(flight);
+		return ResponseEntity.status(HttpStatus.OK).body("Flight is Deleted");
+	}
 }
