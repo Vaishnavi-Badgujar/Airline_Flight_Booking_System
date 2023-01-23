115bdad89d40aa41db197c35ddb7f14a2ae09b31
Sanjivani-source <123052870+Sanjivani-source@users.noreply.github.com>
2023-01-23 18:20:50 +0530

    sanjivaniii added executive

--- src/main/java/com/AirlineFlight/api/Service/ExecutiveService.java
+++ src/main/java/com/AirlineFlight/api/Service/ExecutiveService.java
@@ -1,5 +1,21 @@
 package com.AirlineFlight.api.Service;
 
+import java.util.List;
+
+import com.AirlineFlight.api.model.Executive;
+
 public class ExecutiveService {
+
+	public void insertFlight(Executive executive) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static List<Executive> getAllExecutives() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
--- src/main/java/com/AirlineFlight/api/model/Executive.java
+++ src/main/java/com/AirlineFlight/api/model/Executive.java
@@ -1,8 +1,25 @@
 package com.AirlineFlight.api.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import com.Airline.Flight.api.enums.TicketStatus;
 
 @Entity
 public class Executive {
+
+	@Enumerated(EnumType.STRING)
+	private TicketStatus ticketStatus;
+
+	public TicketStatus getTicketStatus() {
+		return ticketStatus;
+	}
+
+	public void setTicketStatus(TicketStatus ticketStatus) {
+		this.ticketStatus = ticketStatus;
+	}
+	
+	
 
 }
