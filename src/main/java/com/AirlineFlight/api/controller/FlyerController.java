655daabe8bb113ce047ca5fffd4a04f98a371900
savi Dhomne <savivd.21@gmail.com>
2023-01-24 09:59:21 +0530

    added

--- src/main/java/com/AirlineFlight/api/controller/FlyerController.java
+++ src/main/java/com/AirlineFlight/api/controller/FlyerController.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.AirlineFlight.api.Service.FlyerService;
-import com.AirlineFlight.api.model.Airline;
+
 import com.AirlineFlight.api.model.Flyer;
 
 
