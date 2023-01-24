20b3d2a1878173f8eadf8cbeeb53044aa32864a5
Sanjivani-source <123052870+Sanjivani-source@users.noreply.github.com>
2023-01-24 09:13:52 +0530

    Added

--- src/main/java/com/AirlineFlight/api/Service/ExecutiveService.java
+++ src/main/java/com/AirlineFlight/api/Service/ExecutiveService.java
@@ -1,21 +1,25 @@
 package com.AirlineFlight.api.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.AirlineFlight.api.model.Executive;
 
 public class ExecutiveService {
 
-	public void insertFlight(Executive executive) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	public static List<Executive> getAllExecutives() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
+
+	public void insertFlight(Executive executive) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Optional<Executive> getExecutiveByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/AirlineFlight/api/controller/ExceutiveController.java
+++ src/main/java/com/AirlineFlight/api/controller/ExceutiveController.java
@@ -1,37 +1,45 @@
 package com.AirlineFlight.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.AirlineFlight.api.Service.ExecutiveService;
+import com.AirlineFlight.api.Service.FlightService;
 import com.AirlineFlight.api.model.Executive;
-import com.bootapp.rest.restapp.model.Instructor;
-
-
+import com.AirlineFlight.api.model.Flight;
 
 @RestController
 @RequestMapping("/api/executive")
 public class ExceutiveController {
-	
-	
+
 	@Autowired
 	private ExecutiveService executiveService;
-	
-	/* Executive  POST api */
+
+	@Autowired
+	private FlightService flightservice;
+
+
+	/* Executive POST api */
 	@PostMapping("/add")
 	public ResponseEntity<String> postExecutive(@RequestBody Executive executive) {
 		executiveService.insertFlight(executive);
 		return ResponseEntity.status(HttpStatus.OK).body("Executive posted in DB");
 
 	}
+	
+
+
 	/* Get All API */
 	@GetMapping("/getAll")
 	public List<Executive> getAllExecutives() {
@@ -39,6 +47,18 @@ public class ExceutiveController {
 
 		return list;
 	}
-	
+
+	/* Get By Id */
+	@GetMapping("one/{id}")
+	public ResponseEntity<Object> getExecutiveById(@PathVariable("id") int id) {
+		Optional<Executive> optional = executiveService.getExecutiveByID(id);
+		if (!optional.isPresent())
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid ID given");
+
+		Executive executive = optional.get();
+		return ResponseEntity.status(HttpStatus.OK).body(executive);
+	}
+
+
 
 }
