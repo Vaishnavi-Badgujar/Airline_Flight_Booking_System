c7cc380b03e4950ae8e474dd49c67c05f8fd2994
Sanjivani-source <123052870+Sanjivani-source@users.noreply.github.com>
2023-01-24 12:13:58 +0530

    Sanjivani added Excecutive

--- src/main/java/com/AirlineFlight/api/Service/ExecutiveService.java
+++ src/main/java/com/AirlineFlight/api/Service/ExecutiveService.java
@@ -2,24 +2,38 @@ package com.AirlineFlight.api.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.AirlineFlight.api.data.ExecutiveRepository;
 import com.AirlineFlight.api.model.Executive;
 
+@Service
 public class ExecutiveService {
+	@Autowired
+	private ExecutiveRepository executiveRepository;
 
-	public static List<Executive> getAllExecutives() {
-		// TODO Auto-generated method stub
-		return null;
+	public void insertExecutive(Executive executive) {
+		executiveRepository.save(executive);
 	}
 
-	public void insertFlight(Executive executive) {
-		// TODO Auto-generated method stub
-
+	public List<Executive> getAllExecutives() {
+		 
+		 return executiveRepository.findAll();
+		 
 	}
 
-	public Optional<Executive> getExecutiveByID(int id) {
-		// TODO Auto-generated method stub
-		return null;
+
+	public Optional<Executive> getExecutiveById(int id) {
+		Optional<Executive> optional = executiveRepository.findById(id);
+		return optional;
 	}
 
+	public void updateExecutiveById(Executive executive) {
+		executiveRepository.save(executive);
+	}
+
+	public void deleteExecutiveById(Executive executive) {
+		executiveRepository.delete(executive);
+
+	}
 }
--- src/main/java/com/AirlineFlight/api/model/Executive.java
+++ src/main/java/com/AirlineFlight/api/model/Executive.java
@@ -1,13 +1,22 @@
 package com.AirlineFlight.api.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import com.Airline.Flight.api.enums.TicketStatus;
 
 @Entity
 public class Executive {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="Executive_id")
+	private int id;
 
 	@Enumerated(EnumType.STRING)
 	private TicketStatus ticketStatus;
